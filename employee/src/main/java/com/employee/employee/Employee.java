1ea6eecdd42d86984f0be28b3b9987e61d5664b8
Abderrazak <abderrazak.bousselmi.rki@gmail.com>
2019-06-11 17:11:26 +0100

    new project

---
+++ employee/src/main/java/com/employee/employee/Employee.java
@@ -0,0 +1,11 @@
+package com.employee.employee;
+
+import java.io.Serializable;
+
+public class Employee implements 	Serializable  {
+	//il faut utiliser les classes avec majiscule par ce que l'id doit etre un objet 
+	private Long emplyeeId ;
+	private String Name;
+	private String LastName ; 
+
+}
